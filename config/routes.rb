067066e6e9516201ec98958b1eadbf71257dfb5a
daisuke-shimura <168567272+daisuke-shimura@users.noreply.github.com>
2024-06-13 06:55:38 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+
+  root to: 'homes#top'
+  get 'homes/about'
+  resources :users, only: [:show, :edit]
+  resources :books, only: [:new, :index, :show, :edit]
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
