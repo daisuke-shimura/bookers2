9fdb6314277805c056f5cd138cd125d81db0258e
daisuke-shimura <168567272+daisuke-shimura@users.noreply.github.com>
2024-06-20 22:11:33 +0000

    テスト[STEP1]OK

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
- before_action :authenticate_user!, except: [:top]
+ before_action :authenticate_user!, except: [:top ,:about]
  before_action :configure_permitted_parameters, if: :devise_controller?
 
  def after_sign_in_path_for(resource)
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
 
  def after_sign_out_path_for(resouce)
   root_path
+  #redirect_to "home/about"
  end
 
   protected
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   root to: 'homes#top'
-  get '/about' => "homes#about"
+  get 'home/about' => "homes#about"
   #get '/users'=> "users#index"
   resources :users, only: [:show,:index, :edit, :update ]
   resources :books, only: [:new, :index, :show, :edit,:create,:destroy,:update]
