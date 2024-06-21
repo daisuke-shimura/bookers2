b06716b9c61b10ad94ce901796258bc4565c7cf5
daisuke-shimura <168567272+daisuke-shimura@users.noreply.github.com>
2024-06-21 04:54:58 +0000

    [Finish]

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  validates :name, presence: true
+
+  validates :name, uniqueness: true, length: { in: 2..20 }
+  validates :introduction, length: { maximum: 50 }
 
   has_one_attached :image
-  
+
   has_many :books, dependent: :destroy
 
   has_one_attached :profile_image
