6e8c53f979416c39db36291a505ecbc499b95090
daisuke-shimura <daisuke.s0707@icloud.com>
2025-05-06 16:26:57 +0000

    seedファイル作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  {
+    name: "大谷英美里",
+    email: "a@exsample.com",
+    password: "aiueoa"
+  },
+  {
+    name: "齋藤樹愛羅",
+    email: "d@exsample.com",
+    password: "aiueoa"
+  },
+  {
+    name: "佐々木舞香",
+    email: "e@exsample.com",
+    password: "aiueoa"
+  },
+  {
+    name: "高松瞳",
+    email: "f@exsample.com",
+    password: "aiueoa"
+  },
+  {
+    name: "野口衣織",
+    email: "h@exsample.com",
+    password: "aiueoa"
+  }
+])
