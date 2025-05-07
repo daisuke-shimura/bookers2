6aee42d9a0d151bf3db767e42a8371e51705cadb
daisuke-shimura <daisuke.s0707@icloud.com>
2025-05-07 01:00:09 +0000

    コメント 非同期

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,7 +4,6 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    @book.reload
     #redirect_to request.referer
   end
 
@@ -12,7 +11,6 @@ class FavoritesController < ApplicationController
     @book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    @book.reload
     #redirect_to request.referer
   end
 
